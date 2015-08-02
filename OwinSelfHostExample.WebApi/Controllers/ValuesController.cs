dc9c81f5f795601d61dc16076f70a573d89c8631
marciotoshio <contato@marciotoshio.com.br>
2015-08-02 17:02:36 -0300

    adding values controller

---
+++ OwinSelfHostExample.WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web.Http;
+using System.Collections.Generic;
+
+namespace OwinSelfHostExample.WebApi
+{
+	public class ValuesController : ApiController
+	{
+		public IEnumerable<string> Get()
+		{
+			return new[] { "value1", "value2", "value3" };
+		}
+	}
+}
+
