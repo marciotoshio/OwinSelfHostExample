01858dc3f61fcb02e48b53b13456a72ac47c1e30
marciotoshio <contato@marciotoshio.com.br>
2015-08-02 16:48:18 -0300

    startup class

---
+++ OwinSelfHostExample.WebApi/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Owin;
+using System.Web.Http;
+
+namespace OwinSelfHostExample.WebApi
+{
+	public class Startup
+	{
+		public void Configuration(IAppBuilder appBuilder) 
+		{ 
+			var config = new HttpConfiguration(); 
+
+			config.Routes.MapHttpRoute( 
+				name: "DefaultApi", 
+				routeTemplate: "api/{controller}/{id}", 
+				defaults: new { id = RouteParameter.Optional } 
+			); 
+
+			appBuilder.UseWebApi(config); 
+		}
+	}
+}
+
