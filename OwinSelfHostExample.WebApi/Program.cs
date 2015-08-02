4987fa7adec108cd0f9425c2648659f9f7e97e94
marciotoshio <contato@marciotoshio.com.br>
2015-08-02 16:32:27 -0300

    first commit

---
+++ OwinSelfHostExample.WebApi/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+
+namespace OwinSelfHostExample.WebApi
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			var host = "http://localhost:9000";
+			using(WebApp.Start(host))
+			{
+				Console.WriteLine ("Server running on " + host);
+				Console.WriteLine ("Press any key to shutdown.");
+				Console.ReadLine ();
+			}
+		}
+	}
+}
